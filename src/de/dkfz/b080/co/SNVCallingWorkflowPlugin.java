2d251a23fdff6ef89665df5fb4ee006015cddd1e
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2016-03-23 13:52:33 +0100

    Fix missing build*.txt files and recompile.

--- src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
@@ -6,8 +6,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class SNVCallingWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.0.172";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Tue Feb 16 20:45:59 CET 2016";
+    public static final String CURRENT_VERSION_STRING = "1.0.173";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Mar 23 13:52:05 CET 2016";
 
     @Override
     public String getVersionInfo() {
