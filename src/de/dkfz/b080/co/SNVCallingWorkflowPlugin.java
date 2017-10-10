7133c8574a4ed086dc85586e21a2515cfe26a359
warsow <g.warsow@dkfz.de>
2017-10-10 12:17:49 +0200

    included fix for T3028

--- src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
@@ -6,8 +6,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class SNVCallingWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.1.22";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Oct 04 14:47:55 CEST 2017";
+    public static final String CURRENT_VERSION_STRING = "1.1.23";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Tue Oct 10 12:10:24 CEST 2017";
 
     @Override
     public String getVersionInfo() {
