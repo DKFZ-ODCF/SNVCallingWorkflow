80a9b1f587f0431e220151786aac32c3720b5fd7
warsow <g.warsow@dkfz.de>
2017-10-10 11:49:02 +0200

    update version, jar file, etc.

--- src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
@@ -6,8 +6,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class SNVCallingWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.1.20";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Sep 29 10:49:53 CEST 2017";
+    public static final String CURRENT_VERSION_STRING = "1.1.22";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Oct 04 14:47:55 CEST 2017";
 
     @Override
     public String getVersionInfo() {
