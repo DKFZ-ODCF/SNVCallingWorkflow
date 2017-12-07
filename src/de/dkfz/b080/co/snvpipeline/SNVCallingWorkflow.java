35b8bf231709d19dde29348deef564e84bd72716
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-12-07 17:16:55 +0100

    execute() now throws ConfigurationError, according to Roddy changes.

--- src/de/dkfz/b080/co/snvpipeline/SNVCallingWorkflow.java
+++ src/de/dkfz/b080/co/snvpipeline/SNVCallingWorkflow.java
@@ -5,6 +5,7 @@ import de.dkfz.b080.co.files.BamFile;
 import de.dkfz.b080.co.files.SNVAnnotationFile;
 import de.dkfz.b080.co.files.VCFFileGroupForSNVs;
 import de.dkfz.b080.co.files.VCFFileWithCheckpointFile;
+import de.dkfz.roddy.config.ConfigurationError;
 import de.dkfz.roddy.core.ExecutionContext;
 
 /**
@@ -12,7 +13,7 @@ import de.dkfz.roddy.core.ExecutionContext;
 public class SNVCallingWorkflow extends WorkflowUsingMergedBams {
 
     @Override
-    public boolean execute(ExecutionContext context, BamFile bamControlMerged, BamFile bamTumorMerged) {
+    public boolean execute(ExecutionContext context, BamFile bamControlMerged, BamFile bamTumorMerged) throws ConfigurationError {
 
         boolean runMetaCallingStep = context.getConfiguration().getConfigurationValues().getBoolean("runSNVMetaCallingStep", false);
         boolean runDeepAnnotation = context.getConfiguration().getConfigurationValues().getBoolean("runDeepAnnotation", true);
