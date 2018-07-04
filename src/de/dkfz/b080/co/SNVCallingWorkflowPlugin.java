3b9ef350d81eafb36e13cfd1d19af3bec4f23405
warsow <g.warsow@dkfz.de>
2018-07-04 15:39:49 +0200

    switched to COWorkflowsBasePlugin:1.0.3 and roddy 3.2

--- src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SNVCallingWorkflowPlugin.java
@@ -7,7 +7,7 @@ import de.dkfz.roddy.plugins.BasePlugin;
 public class SNVCallingWorkflowPlugin extends BasePlugin {
 
     public static final String CURRENT_VERSION_STRING = "1.3.0";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Apr 18 14:50:59 CEST 2018";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Tue Jul 03 16:46:06 CEST 2018";
 
     @Override
     public String getVersionInfo() {
