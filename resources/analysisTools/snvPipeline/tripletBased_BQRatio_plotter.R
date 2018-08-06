3eb45fe8344c113e29f3dab53aecde80aa61ca0b
warsow <g.warsow@dkfz.de>
2018-08-06 13:19:40 +0200

    fix bug T3719
    Summary: fixed bug T3719
    
    Test Plan: checkout and test
    
    Reviewers: seufert
    
    Reviewed By: seufert
    
    Maniphest Tasks: T3719
    
    Differential Revision: https://eilslabs-phabricator.dkfz.de/D498

--- resources/analysisTools/snvPipeline/tripletBased_BQRatio_plotter.R
+++ resources/analysisTools/snvPipeline/tripletBased_BQRatio_plotter.R
@@ -205,7 +205,11 @@ for (triplet in rownames(tripletSpecificBaseQualities.ref)) {
   tripletSpecificBaseQualityRatios[triplet,"diff.median"] = median.alt - median.ref  
   
   if(length(scores.alt)>1 & length(scores.ref)>1) {
-    tripletSpecificBaseQualityRatios[triplet,"tTest_pval"] = t.test(scores.alt, scores.ref)$p.value
+    if (sum(abs(scores.alt - scores.ref)) == 0) {
+      tripletSpecificBaseQualityRatios[triplet,"tTest_pval"] = 1
+    } else {
+      tripletSpecificBaseQualityRatios[triplet,"tTest_pval"] = t.test(scores.alt, scores.ref)$p.value
+    }
   } else {
     if(length(scores.alt)>1 & length(scores.ref)==1) {
       tripletSpecificBaseQualityRatios[triplet,"tTest_pval"] = t.test(scores.alt, mu = scores.ref)$p.value
