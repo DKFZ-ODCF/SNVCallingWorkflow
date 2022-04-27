41ac61fac7fbb58d56b36de0aeffc6b0c3023d6d
Philip R. Kensche <p.kensche@dkfz.de>
2022-04-27 17:13:32 +0200

    Removed obsolete usePypy variable (determined from executable). Warn about broken cluster environment.

--- resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster.sh
+++ resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster.sh
@@ -18,9 +18,11 @@ module load perl/"${PERL_VERSION:?PERL_VERSION undefined}"
 module load bedtools/"${BEDTOOLS_VERSION:?BEDTOOLS_VERSION undefined}"
 module load pypy/"${PYPY_VERSION:?PYPY_VERSION undefined}"
 
+echo "Environment is broken for module python/2.7.9. Use pypy or Conda environment" >> /dev/stderr
 set +ue
 source /dkfz/cluster/virtualenvs/paramasi/python_2.7.9_SNVCalling_pysam_0.16.0.1/bin/activate
 set -ue
+
 
 export BGZIP_BINARY=bgzip
 export TABIX_BINARY=tabix
