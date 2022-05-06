933aaef1f93e83b7bf54032642e2624b8fe783c8
Philip R. Kensche <p.kensche@dkfz.de>
2022-05-06 16:14:28 +0200

    Reverted to master version of tbi-lsf-cluster_snvAnnotation.sh. The previous changes won't be used. We drop pypy support.

--- resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster_snvAnnotation.sh
+++ resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster_snvAnnotation.sh
@@ -15,11 +15,7 @@ export HTSLIB_INCLUDE_PATH="/tbi/software/x86_64/htslib/htslib-$HTSLIB_VERSION_F
 export PYPY_LOCAL_LIBPATH="$HOME/.local/lib/pypy/hts-python/$HTS_PYTHON_COMMIT"
 
 usePypy() {
-    # shellcheck disable=SC1073
-    if [[ "$(which "$PYPY_OR_PYTHON_BINARY" 2> /dev/null)" == "" ]]; then
-        echo "Couldn't find binary '$PYPY_OR_PYTHON_BINARY'" >> /dev/stderr
-        exit 1
-    elif [[ $("$PYPY_OR_PYTHON_BINARY" --version 2>&1 | grep -P "PyPy") != "" ]]; then
+    if [[ $("$PYPY_OR_PYTHON_BINARY" --version 2>&1 | grep -P "PyPy") != "" ]]; then
         echo true
     else
         echo false
@@ -53,51 +49,28 @@ cloneAndBuildHtsPython() {
     local HTS_PYTHON_REPODIR="$PYPY_LOCAL_LIBPATH/repository"
     if [[ ! -d "$sitePackageDir" ]]; then
         mkdir -p "$sitePackageDir"
-    fi
-    local LOCK="$sitePackageDir/hts-egg~"
-    lock "$LOCK"
-    # Registering a trap to ensure that the lock is removed also in case of an error. To not
-    # pollute the outer trap namespace, the block is put into a subshell by parentheses.
-    (
-        trap "echo 'Forced unlock: $LOCK' >> /dev/stderr; unlock $LOCK" ERR
-        # The trap must be set after the lock was successfully acquired, or it may delete a lock
-        # set by another process.
+        local LOCK="$sitePackageDir/hts-egg~"
+        lock "$LOCK"
 
-        if [[ $(ls -d $sitePackageDir/hts-*.egg 2>> /dev/null | wc -l) -eq 0 ]]; then
-            module load git/"${GIT_VERSION:?GIT_VERSION undefined}"
-            export GIT_BINARY=git
+        module load git/"${GIT_VERSION:?GIT_VERSION undefined}"
+        export GIT_BINARY=git
 
-            if [[ ! -d "$(echo "$HTS_PYTHON_REPODIR")" ]]; then
-                echo "Cloning hts-python repository" >> /dev/stderr
-                "$GIT_BINARY" clone "$HTS_PYTHON_GIT_REPOSITORY" "$HTS_PYTHON_REPODIR"
-                "$GIT_BINARY" -C "$HTS_PYTHON_REPODIR" checkout "$HTS_PYTHON_COMMIT"
-            fi
+        # Needed for CFFI
+        module switch "htslib/$HTSLIB_VERSION_FOR_HTS_PYTHON"
+
+        if [[ ! -d `echo "$sitePackageDir/hts-*.egg"` ]]; then
+            "$GIT_BINARY" clone "$HTS_PYTHON_GIT_REPOSITORY" "$HTS_PYTHON_REPODIR"
+            "$GIT_BINARY" -C "$HTS_PYTHON_REPODIR" checkout "$HTS_PYTHON_COMMIT"
+            # Below assume that 'nose' is already installed with PyPy
             pushd "$HTS_PYTHON_REPODIR"
-
-            if [[ $(ls .eggs/nose* 2> /dev/null | wc -l) -eq 0 ]]; then
-                echo "Installing nose (needed for installation)" >> /dev/stderr
-                pip3 install -t .eggs/ nose==1.3.7
-            fi
-
-            # Needed for CFFI
-            module switch "htslib/$HTSLIB_VERSION_FOR_HTS_PYTHON"
-
-            echo "Building and installing hts-python for pypy" >> /dev/stderr
-            C_INCLUDE_PATH="$HTSLIB_INCLUDE_PATH" PYTHONPATH="$sitePackageDir" \
-              "$PYPY_OR_PYTHON_BINARY" setup.py build
-            C_INCLUDE_PATH="$HTSLIB_INCLUDE_PATH" PYTHONPATH="$sitePackageDir" \
-              "$PYPY_OR_PYTHON_BINARY" setup.py install --prefix="$PYPY_LOCAL_LIBPATH"
-
+            C_INCLUDE_PATH="$HTSLIB_INCLUDE_PATH" PYTHONPATH="$sitePackageDir" "$PYPY_OR_PYTHON_BINARY" setup.py build
+            C_INCLUDE_PATH="$HTSLIB_INCLUDE_PATH" PYTHONPATH="$sitePackageDir" "$PYPY_OR_PYTHON_BINARY" setup.py install --prefix="$PYPY_LOCAL_LIBPATH"
             popd
-            unset C_INCLUDE_PATH
-            module switch "htslib/$HTSLIB_VERSION"
-        else
-            echo "hts-python for pypy is already installed" >> /dev/stderr
         fi
+        unset C_INCLUDE_PATH
+        module switch "htslib/$HTSLIB_VERSION"
         unlock "$LOCK"
-        # Delete the trap
-        trap - ERR
-    )
+    fi
 }
 
 pypyCopySam() {
