13bdc5f5957299b30c84a54d775247d8afcf5386
paramasi <n.paramasivam@dkfz.de>
2023-03-23 09:07:48 +0100

    Move python env

--- resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster.sh
+++ resources/analysisTools/snvPipeline/environments/tbi-lsf-cluster.sh
@@ -19,7 +19,7 @@ module load bedtools/"${BEDTOOLS_VERSION:?BEDTOOLS_VERSION undefined}"
 module load pypy/"${PYPY_VERSION:?PYPY_VERSION undefined}"
 
 set +ue
-source /dkfz/cluster/virtualenvs/paramasi/python_2.7.9_SNVCalling_pysam_0.16.0.1/bin/activate
+source /omics/groups/OE0246/shared/paramasi/compute_env/python_2.7.9_SNVCalling_pysam_0.16.0.1/bin/activate
 set -ue
 
 export BGZIP_BINARY=bgzip
